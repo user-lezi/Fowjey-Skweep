f317781719dcfeb0e9b91b1e65dfe356b3b1dba9
user-lezi <leleziar@gmail.com>
2026-02-07 11:26:13 +0530

    installed forgecolor

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,13 @@
 import { ForgeCanvas } from "@tryforge/forge.canvas";
 import { ForgeClient, LogPriority } from "@tryforge/forgescript";
 import { config } from "dotenv";
+import { ForgeColor } from "forge.color";
 
 config({ quiet: true });
 const client = new ForgeClient({
   intents: ["GuildMembers", "GuildMessages", "Guilds", "MessageContent"],
   events: ["clientReady", "messageCreate", "interactionCreate"],
-  extensions: [new ForgeCanvas()],
+  extensions: [new ForgeCanvas(), new ForgeColor()],
   mobile: true,
   logLevel: LogPriority.High,
   prefixes: [process.env.Prefix!, "~"],
