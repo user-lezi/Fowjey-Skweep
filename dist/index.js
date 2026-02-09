a4a964c7585ea04e4696d68b5624c174fa50a5b1
user-lezi <leleziar@gmail.com>
2026-02-09 09:22:35 +0530

    init forgecolor and forgeuser

--- dist/index.js
+++ dist/index.js
@@ -3,11 +3,17 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const forge_canvas_1 = require("@tryforge/forge.canvas");
 const forgescript_1 = require("@tryforge/forgescript");
 const dotenv_1 = require("dotenv");
+const forge_color_1 = require("forge.color");
+const forge_user_1 = require("forge.user");
 (0, dotenv_1.config)({ quiet: true });
+const userBot = new forge_user_1.ForgeUser({
+    token: process.env.UserToken,
+    events: ["ready", "messageCreate"],
+});
 const client = new forgescript_1.ForgeClient({
     intents: ["GuildMembers", "GuildMessages", "Guilds", "MessageContent"],
     events: ["clientReady", "messageCreate", "interactionCreate"],
-    extensions: [new forge_canvas_1.ForgeCanvas()],
+    extensions: [new forge_canvas_1.ForgeCanvas(), new forge_color_1.ForgeColor(), userBot],
     mobile: true,
     logLevel: forgescript_1.LogPriority.High,
     prefixes: [process.env.Prefix, "~"],
--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,18 @@ import { ForgeCanvas } from "@tryforge/forge.canvas";
 import { ForgeClient, LogPriority } from "@tryforge/forgescript";
 import { config } from "dotenv";
 import { ForgeColor } from "forge.color";
+import { ForgeUser } from "forge.user";
 
 config({ quiet: true });
+const userBot = new ForgeUser({
+  token: process.env.UserToken,
+  events: ["ready", "messageCreate"],
+});
+
 const client = new ForgeClient({
   intents: ["GuildMembers", "GuildMessages", "Guilds", "MessageContent"],
   events: ["clientReady", "messageCreate", "interactionCreate"],
-  extensions: [new ForgeCanvas(), new ForgeColor()],
+  extensions: [new ForgeCanvas(), new ForgeColor(), userBot],
   mobile: true,
   logLevel: LogPriority.High,
   prefixes: [process.env.Prefix!, "~"],
